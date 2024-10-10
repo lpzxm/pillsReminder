78f48cafd4ddab140e6cf39b29b08d0d2fc45600
Adrián López <ja044840@gmail.com>
2024-10-10 10:17:15 -0600

    404

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Error404 } from './components/common/Error404.jsx';
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: (
+      <>
+        <Error404 />
+      </>
+    ),
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+      <RouterProvider router={router} />
+  </React.StrictMode>
 )

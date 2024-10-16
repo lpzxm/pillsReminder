8f24e58a76fe17b51cc2d776bd4121d3ac24712d
lpzxm <ja044840@gmail.com>
2024-10-15 23:08:01 -0600

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,6 @@ import { Error404 } from './components/common/Error404.jsx';
 import { HomePage } from "./pages/homePage.jsx";
 import { AddAlarm } from "./components/common/alarms/addAlarms.jsx";
 import { ViewAlarms } from "./components/common/alarms/viewAlarms.jsx";
-import { EditAlarms } from "./components/common/alarms/editAlarms.jsx";
 
 const router = createBrowserRouter([
   {
@@ -38,14 +37,6 @@ const router = createBrowserRouter([
     element: (
       <>
         <AddAlarm />
-      </>
-    ),
-  },
-  {
-    path: "/alarms/editAlarms/:id",
-    element: (
-      <>
-        <EditAlarms />
       </>
     ),
   },
